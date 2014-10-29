d9c9152283eae82f2636471a2813bebc02c722a3
thohag <tmshag1@gmail.com>
2014-10-29 16:56:20 +0100

    Added regex pattern matching for files.

--- R/readFolder_function.R
+++ R/readFolder_function.R
@@ -10,7 +10,7 @@
 #' dir = "c:/experiments/experiment1/"
 #' data = readFolder(dir)
 
-readFolder = function(dir, sep ="\t", format = "data.frame", header = T, fun) {
+readFolder = function(dir, pattern = "*", sep = "\t", format = "data.frame", header = T, fun) {
   # reads all files in directory and combines to one dataframe
   # param
   #   dir = string describing directory
@@ -20,17 +20,18 @@ readFolder = function(dir, sep ="\t", format = "data.frame", header = T, fun) {
   #Check parameters
   if (!is.character(dir)) stop("dir needs to be of type character")
   if (!file.exists(dir)) stop(paste("dir '",dir,"' does not exist",sep=""))
-  if (length(list.files(dir)) == 0) stop("no files in dir")
+  if (length(list.files(dir, pattern = glob2rx(pattern))) == 0) stop(paste("no files in dir matching",pattern))
   if (!is.character(sep)) stop("sep needs to be of type character")
   if (!is.character(format)) stop("format needs to be of type character")
   if (!is.logical(header)) stop("header needs to be of type logical")
+  if (!is.character(pattern)) stop("pattern needs to be of type character")
   
   #Remove trailing file separation
   if (substring(dir,nchar(dir)) == .Platform$file.sep) {
     dir = substring(dir,1,nchar(dir)-1)
   }
   
-  files = paste(dir, list.files(path = dir), sep = .Platform$file.sep) # all files in dir
+  files = paste(dir, list.files(path = dir, pattern = glob2rx(pattern)), sep = .Platform$file.sep) # all files in dir
   
   dataList = lapply(files, read.table, sep = sep, header = header) # files to list with [[]] for each subjects
   
