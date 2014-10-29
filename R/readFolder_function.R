d8a6081f3c85520903433b1479ce92f378010346
thohag <tmshag1@gmail.com>
2014-10-29 14:40:21 +0100

    File separation fix.

--- R/readFolder_function.R
+++ R/readFolder_function.R
@@ -10,12 +10,13 @@
 #' data = readFolder(dir)
 
 readFolder = function(dir, sep ="\t", format = "data.frame", header = T) {
-  # reads all files in directory and combines to one dataframe
-  # param
-  #   dir = string describing directory
-  # return
-  #   dataframe containing all subjects
-  files = paste(dir, list.files(path = dir), sep = "") # all files in dir
+  
+  #Remove trailing file separation
+  if (substring(dir,nchar(dir)) == .Platform$file.sep) {
+    dir = substring(dir,1,nchar(dir)-1)
+  }
+  
+  files = paste(dir, list.files(path = dir), sep = .Platform$file.sep) # all files in dir
   
   dataList = lapply(files, read.table, sep = sep, header = header) # files to list with [[]] for each subjects
   
